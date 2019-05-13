5e577453d10915bbffbedbc8a8b1ce7d953009d3
BagelEnthusiast <nathan.richardson94@gmail.com>
2019-05-13 17:58:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName){
+   let returnArray = []
+for (let index = 0; index < namesArray.length; index++) {
+   returnArray.push(`Thank you, ${namesArray[index]}, for the wonderful ${eventName} gift!`) 
+
+}
+return returnArray
+}
+
+function countdown(integer)
+{
+while (integer >= 0) {
+console.log(integer)
+integer--
+}
+}
+
+console.log(countdown(20))
